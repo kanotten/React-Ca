f2cbd31821f09a3226cc89c89decb0f01a044317
Sheikh <kenneth.sheikh@hotmail.com>
2025-03-09 22:53:02 +0100

    added cart item counter on top of cart navbar option

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
+import { useCart } from "../context/useCart";
 
 export default function Navbar() {
+  const { cart } = useCart();
+
+  // 🔹 Calculate total number of items (including duplicates)
+  const totalItems = cart.length;
+
   return (
     <nav className="bg-gray-900 text-white p-4">
       <ul className="flex justify-center gap-6 text-gray-400">
@@ -9,11 +15,20 @@ export default function Navbar() {
             Home
           </Link>
         </li>
-        <li>
-          <Link to="/cart" className="hover:text-white transition-colors">
+        <li className="relative">
+          <Link
+            to="/cart"
+            className="hover:text-white transition-colors relative"
+          >
             Cart
+            {totalItems > 0 && (
+              <span className="absolute -top-3 -right-3 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
+                {totalItems}
+              </span>
+            )}
           </Link>
         </li>
+
         <li>
           <Link to="/contact" className="hover:text-white transition-colors">
             Contact
