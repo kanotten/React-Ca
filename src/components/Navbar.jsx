8a1862b9712d1e8bd9e6cac18e135fc941b83cc4
Sheikh <kenneth.sheikh@hotmail.com>
2025-03-09 16:27:29 +0100

    fixed router and tailwind error for pages folder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,35 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <nav className="bg-blue-600 text-white p-4">
-      <h1 className="text-lg font-bold">MyStore</h1>
+      <ul className="flex gap-4">
+        <li>
+          <Link to="/" className="hover:underline">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/product/1" className="hover:underline">
+            Product
+          </Link>
+        </li>
+        <li>
+          <Link to="/cart" className="hover:underline">
+            Cart
+          </Link>
+        </li>
+        <li>
+          <Link to="/checkout-success" className="hover:underline">
+            Checkout
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" className="hover:underline">
+            Contact
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 }
