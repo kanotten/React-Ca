6632fecd774bb87b92a43d46ab3f03c2dc92262f
Sheikh <kenneth.sheikh@hotmail.com>
2025-03-09 22:37:25 +0100

    fixed so removing single product of same stack is correct

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -10,7 +10,15 @@ export const CartProvider = ({ children }) => {
   };
 
   const removeFromCart = (id) => {
-    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+    setCart((prevCart) => {
+      const index = prevCart.findIndex((item) => item.id === id);
+      if (index !== -1) {
+        const updatedCart = [...prevCart];
+        updatedCart.splice(index, 1);
+        return updatedCart;
+      }
+      return prevCart;
+    });
   };
 
   const clearCart = () => setCart([]);
