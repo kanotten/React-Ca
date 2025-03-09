f73ce21a5e2786b8f2310e29a854575e088e585d
Sheikh <kenneth.sheikh@hotmail.com>
2025-03-09 16:50:09 +0100

    retrieved data from api on home

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,9 +3,24 @@ import { useState, useEffect } from "react";
 export default function Homepage() {
   const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await fetch("https://v2.api.noroff.dev/online-shop");
+        const data = await response.json();
+        setProducts(data.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <h1 className="text-2xl font-bold p-4">Produkter</h1>
+      <pre>{JSON.stringify(products, null, 2)}</pre>
     </div>
   );
 }
