4f68028f8c670be304c7b4ff33e6bb8d8748f6c7
Sheikh <kenneth.sheikh@hotmail.com>
2025-03-09 16:40:40 +0100

    fixed header slighly

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,29 +3,32 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="bg-gray-900 text-white p-4">
-      <ul className="flex gap-4">
+      <ul className="flex justify-center gap-6 text-gray-400">
         <li>
-          <Link to="/" className="hover:underline">
+          <Link to="/" className="hover:text-white transition-colors">
             Home
           </Link>
         </li>
         <li>
-          <Link to="/product/1" className="hover:underline">
+          <Link to="/product/1" className="hover:text-white transition-colors">
             Product
           </Link>
         </li>
         <li>
-          <Link to="/cart" className="hover:underline">
+          <Link to="/cart" className="hover:text-white transition-colors">
             Cart
           </Link>
         </li>
         <li>
-          <Link to="/checkout-success" className="hover:underline">
+          <Link
+            to="/checkout-success"
+            className="hover:text-white transition-colors"
+          >
             Checkout
           </Link>
         </li>
         <li>
-          <Link to="/contact" className="hover:underline">
+          <Link to="/contact" className="hover:text-white transition-colors">
             Contact
           </Link>
         </li>
